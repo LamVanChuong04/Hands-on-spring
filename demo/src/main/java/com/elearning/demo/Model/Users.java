edf15e24b2cb332ed4ad813f4d278c869f1e90d2
Lâm Văn Chương <lamvanchuong04@gmail.com>
2026-03-24 20:01:50 +0700

    #4 perf(user): resolve N+1 query problem

--- demo/src/main/java/com/elearning/demo/Model/Users.java
+++ demo/src/main/java/com/elearning/demo/Model/Users.java
@@ -5,6 +5,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.BatchSize;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
@@ -30,6 +31,7 @@ public class Users implements UserDetails {
     private String password;
 
     @OneToMany(mappedBy = "user",fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @BatchSize(size = 5)
     private List<PostModel> posts;
 
 
--- demo/src/main/java/com/elearning/demo/Service/UserService.java
+++ demo/src/main/java/com/elearning/demo/Service/UserService.java
@@ -1,21 +1,27 @@
 package com.elearning.demo.Service;
 
+import com.elearning.demo.Dto.Response.PostResponse;
+import com.elearning.demo.Dto.Response.UserResponse;
 import com.elearning.demo.Model.Users;
+import com.elearning.demo.Repository.PostRepository;
 import com.elearning.demo.Repository.UserRepository;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class UserService {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
-
-    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+    private final PostRepository postRepository;
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder,  PostRepository postRepository) {
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
+        this.postRepository = postRepository;
     }
 
     public String addUser(Users user) {
@@ -25,8 +31,24 @@ public class UserService {
         userRepository.save(newUser);
         return "Add user successfully!";
     }
-    public List<Users> getAllUsers() {
-        return userRepository.findAll();
+    public List<UserResponse> getAllUsers() {
+        List<Users> users = userRepository.findAll();
+
+        return users.stream().map(user -> {
+            UserResponse userResponse = new UserResponse();
+            userResponse.setUsername(user.getUsername());
+
+            List< PostResponse> posts = user.getPosts().stream().map(post ->{
+                PostResponse postResponse = new PostResponse();
+                postResponse.setPostContent(post.getPostContent());
+                postResponse.setPostTitle(post.getPostTitle());
+                return postResponse;
+            }).toList();
+
+            userResponse.setPosts(posts);
+            return userResponse;
+
+        }).collect(Collectors.toList());
     }
 
     public Optional<Users> getUserByUsername(String username) {
